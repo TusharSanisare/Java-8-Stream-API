a98cab4418523c215156f80dc613788cfac2365e
TusharSanisare <121483380+TusharSanisare@users.noreply.github.com>
2025-05-18 16:21:20 +0530

    p1

---
+++ Problem_01.java
@@ -0,0 +1,20 @@
+/**
+ * Problem Statement: 
+ * Given a sentance find the word in it which have highest leangh
+ * 
+ * eg-> I/P: This is the problem statement for this question
+ *      O/P: statement
+ * 
+ */
+
+/** Start your solution from here */
+class Problem_01{
+    public String problem_01(String str){
+        /** complete the function --- use java stream api */
+    }
+}
+
+/**
+ * 
+ * 
+ */
